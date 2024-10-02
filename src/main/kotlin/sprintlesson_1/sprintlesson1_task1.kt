23fab1d74f2c14336210bec045193df4af222f26
elena <testermermaid@gmail.com>
2024-10-02 16:35:19 +0200

    SprintLesson-1 Added indents.

--- src/main/kotlin/sprintlesson_1/sprintlesson1_task1.kt
+++ src/main/kotlin/sprintlesson_1/sprintlesson1_task1.kt
@@ -2,9 +2,9 @@ package sprintlesson_1
 
 fun main() {
 
-val numberOfOrders = 75
+    val numberOfOrders = 75
 
-val notificationAboutPurchase = "Спасибо за покупку!"
+    val notificationAboutPurchase = "Спасибо за покупку!"
 
     println(numberOfOrders)
     println(notificationAboutPurchase)
