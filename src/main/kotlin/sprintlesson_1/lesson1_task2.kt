df0b29799d0c5b34d6c122dc7e8a5eb78e4cf0db
elena <testermermaid@gmail.com>
2024-10-02 16:46:59 +0200

    KS-1-2 Added var numberOfEmployees

---
+++ src/main/kotlin/sprintlesson_1/lesson1_task2.kt
@@ -0,0 +1,20 @@
+package sprintlesson_1
+
+fun main() {
+
+    val numberOfOrders = 75
+
+    val notificationAboutPurchase = "Спасибо за покупку!"
+
+    println(numberOfOrders)
+    println(notificationAboutPurchase)
+
+    var numberOfEmployees = 2000
+
+//    println(numberOfEmployees)
+
+    numberOfEmployees = 1999
+
+    println(numberOfEmployees)
+
+}
