71021506983e29cbb9e995845eba1f702b476851
elena <testermermaid@gmail.com>
2024-10-07 19:13:52 +0200

    KS 2-4 Task about BAFF

---
+++ src/main/kotlin/sprintlesson_2/lesson2_task4.kt
@@ -0,0 +1,14 @@
+package sprintlesson_2
+
+//почему-то не нравится название переменной
+
+const val BAFF = 0.2
+
+fun main() {
+
+    val crystal = 7
+    val iron = 11
+    println((crystal * BAFF).toInt())
+    println((iron * BAFF).toInt())
+
+}
