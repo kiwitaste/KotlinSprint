e6ef70295734ea96f718e943f4998df9e2ef2097
elena <testermermaid@gmail.com>
2024-10-07 19:29:51 +0200

    KS 2-3 Renamed val departureHourTimeInMinutes

--- src/main/kotlin/sprintlesson_2/lesson2_task3.kt
+++ src/main/kotlin/sprintlesson_2/lesson2_task3.kt
@@ -5,10 +5,10 @@ const val TIME_IN_MINUTES = 60
 fun main() {
 
     val departureTimeHour = 9
-    val departureTimeMinutes = departureTimeHour * TIME_IN_MINUTES // преобразую часы в минуты, вопрос как лучше назвать переменную в таком случае?
+    val departureHourTimeInMinutes = departureTimeHour * TIME_IN_MINUTES // преобразую часы в минуты, вопрос как лучше назвать переменную в таком случае?
     val departureTimeMinute = 39
     val travelTimeMinutes = 457
-    val arrivalTimeMinutes = departureTimeMinutes + departureTimeMinute + travelTimeMinutes
+    val arrivalTimeMinutes = departureHourTimeInMinutes + departureTimeMinute + travelTimeMinutes
     val resultHour = arrivalTimeMinutes / TIME_IN_MINUTES
     val resultMinute = arrivalTimeMinutes % TIME_IN_MINUTES
     val totalResult = ("$resultHour:$resultMinute")
