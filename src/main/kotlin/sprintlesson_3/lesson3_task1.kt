0fbaf0772e3806f570e90de445160fb58283351b
elena <testermermaid@gmail.com>
2024-10-07 19:43:29 +0200

    KS 3-1 Task about greeting

---
+++ src/main/kotlin/sprintlesson_3/lesson3_task1.kt
@@ -0,0 +1,12 @@
+package sprintlesson_3
+
+fun main() {
+
+    val userName = "Александр"
+    var timeOfTheDay = "Добрый день"
+    println("$timeOfTheDay, $userName!")
+
+    timeOfTheDay = "Добрый вечер"
+    println("$timeOfTheDay, $userName!")
+
+}
