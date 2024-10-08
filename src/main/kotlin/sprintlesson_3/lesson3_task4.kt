149d6054d3fad6ead2fb41ad1613c8fdc8a98e59
elena <testermermaid@gmail.com>
2024-10-08 11:02:35 +0200

    KS 3-4 Task about chess

---
+++ src/main/kotlin/sprintlesson_3/lesson3_task4.kt
@@ -0,0 +1,18 @@
+package sprintlesson_3
+
+fun main() {
+
+    var from = "E2"
+    var to = "E4"
+    var moveNumber = 1
+
+
+    println("$from-$to;$moveNumber")
+
+    from = "D2"
+    to = "D3"
+    moveNumber = 2
+
+    println("$from-$to;$moveNumber")
+
+}
